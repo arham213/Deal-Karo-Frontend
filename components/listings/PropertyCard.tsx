650b69f0fe867e811aaff034ef262ac93d0ac921
arham213 <76661263+arham213@users.noreply.github.com>
2025-11-11 03:24:39 +0500

    feat:UI-Figma-Matching

---
+++ components/listings/PropertyCard.tsx
@@ -0,0 +1,261 @@
+import { Colors } from "@/constants/colors";
+import { fontFamilies, fontSizes, fontWeights, radius, spacing } from "@/styles";
+import { User } from "@/types/auth";
+import { ListingState } from "@/types/listings";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { AvatarInitials } from "../AvatarInitials";
+
+export const PropertyCard = ({ property, user, handlePropertyDetails }: { property: ListingState, user: User, handlePropertyDetails: (listingId: string) => void }) => (
+    <View style={styles.propertyCard}>
+      {/* Content */}
+      <View style={styles.propertyContent}>
+        {/* Header with Title and Status */}
+        <View style={styles.headerWithLocation}>
+        <View style={styles.propertyHeader}>
+          <View style={styles.titleSection}>
+            <Text style={styles.propertyTitle}>
+              {property.area} {property.propertyType}
+            </Text>
+          </View>
+          {/* {property.} */}
+          {property.listingType === "rent" ? (
+            <Text style={styles.price}>Rs. {property.rentPerMonth}/month</Text>
+          ) : (
+            <Text style={styles.price}>Rs. {property.price || property.totalPrice}</Text>
+          )}
+        </View>
+
+        {user?.verificationStatus === "verified"  && (
+              <View style={styles.locationRow}>
+                <Ionicons name="location" size={12} color={Colors.textSecondary} />
+                <Text style={styles.propertyLocation}>
+                  {property.phase}, {property.block}
+                </Text>
+              </View>
+            )}
+        </View>
+
+        {/* Meta Information */}
+        <View style={styles.propertyMetaRow}>
+          <View>
+            {/* Added By */}
+            <View style={styles.addedByContainer}>
+              <AvatarInitials name={property.userId?.name || 'Unknown'} size={32} backgroundColor={Colors.neutral30} textColor={Colors.text} />
+              <View style={styles.addedByDetailsContainer}>
+                <Text style={styles.addedByLabel}>Added by</Text>
+                <Text style={styles.addedBy}>{property.userId?.name || 'Unknown'}</Text>
+              </View>
+            </View>
+          </View>
+          <View style={[property.listingType === "cash" ? styles.statusBadgeCash: styles.statusBadgeInstallments]}>
+            <Text style={[property.listingType === "cash" ? styles.statusTextCash : styles.statusTextInstallments]}>{property.listingType === "cash" ? "Cash" : "Installments"}</Text>
+          </View>
+        </View>
+
+        {/* Action Buttons */}
+        {user?.verificationStatus === "verified"  && (
+          <View style={styles.actionButtons}>
+            <TouchableOpacity style={styles.detailsButton} onPress={() => handlePropertyDetails(property._id)}>
+              <Ionicons name="information-circle-outline" size={16} color={Colors.textSecondary} />
+              <Text style={styles.actionButtonText}>Details</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.contactButton}>
+              <Ionicons name="call-outline" size={16} color={Colors.textSecondary} />
+              <Text style={styles.actionButtonText}>Contact</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
+    </View>
+  )
+
+  const styles = StyleSheet.create({
+    propertyCard: {
+      backgroundColor: Colors.neutral10,
+      borderRadius: radius.lg,
+      borderWidth: 1,
+      borderColor: Colors.neutral40,
+      overflow: "hidden",
+      marginHorizontal: spacing.screen,
+      marginBottom: spacing.md,
+    },
+    propertyImageContainer: {
+      height: 140,
+      backgroundColor: Colors.inputBackground,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    propertyImage: {
+      width: "100%",
+      height: "100%",
+    },
+    propertyContent: {
+      padding: spacing.lg,
+      gap: spacing.lg,
+    },
+    headerWithLocation: {
+      flex: 1,
+      gap: spacing.sm
+    },
+    propertyHeader: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "flex-start",
+    },
+    titleSection: {
+      flex: 1,
+      gap: spacing.sm,
+    },
+    propertyTitle: {
+      fontSize: fontSizes.base,
+      fontWeight: fontWeights.bold,
+      color: Colors.black,
+      fontFamily: fontFamilies.primary,
+      fontStyle: "normal",
+    },
+    locationRow: {
+      flexDirection: "row",
+      alignItems: "center",
+      gap: 4,
+    },
+    propertyLocation: {
+      fontSize: fontSizes.xs,
+      color: Colors.neutral80,
+      fontFamily: fontFamilies.primary,
+      fontStyle: "normal",
+      fontWeight: fontWeights.medium,
+      letterSpacing: 0.12,
+    },
+    statusBadgeCash: {
+      paddingVertical: spacing.xs,
+      paddingHorizontal: spacing.sm,
+      borderRadius: radius.lg,
+      backgroundColor: Colors.backgroundCash,
+    },
+    statusTextCash: {
+      fontSize: fontSizes.xs,
+      fontWeight: fontWeights.medium,
+      color: Colors.textCash,
+      fontFamily: fontFamilies.primary,
+      fontStyle: "normal",
+      letterSpacing: 0.12,
+    },
+    statusBadgeInstallments: {
+      paddingVertical: spacing.xs,
+      paddingHorizontal: spacing.sm,
+      borderRadius: radius.lg,
+      backgroundColor: Colors.backgroundInstallments,
+    },
+    statusTextInstallments: {
+      fontSize: fontSizes.xs,
+      fontWeight: fontWeights.medium,
+      color: Colors.textInstallments,
+      fontFamily: fontFamilies.primary,
+      fontStyle: "normal",
+      letterSpacing: 0.12,
+    },
+    propertyMetaRow: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    addedByLabel: {
+      fontSize: fontSizes.xs,
+      color: Colors.neutral80,
+      fontFamily: fontFamilies.primary,
+      fontStyle: "normal",
+      fontWeight: fontWeights.medium,
+      letterSpacing: 0.12,
+    },
+    price: {
+      fontSize: fontSizes.base,
+      fontWeight: fontWeights.semibold,
+      color: Colors.black,
+      fontFamily: fontFamilies.primary,
+      fontStyle: "normal",
+    },
+    addedByDetailsContainer: {
+      flexDirection: "column",
+      gap: spacing.xxxs,
+    },
+    addedByContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      gap: spacing.sm,
+    },
+    addedBy: {
+      fontSize: fontSizes.sm,
+      color: Colors.neutral100,
+      fontFamily: fontFamilies.primary,
+      fontStyle: "normal",
+      fontWeight: fontWeights.semibold,
+      letterSpacing: 0.14,
+    },
+    actionButtons: {
+      flexDirection: "row",
+      marginTop: spacing.sm,
+      gap: spacing.sm,
+    },
+    detailsButton: {
+      flex: 1,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+      gap: 6,
+      paddingVertical: 6,
+    },
+    contactButton: {
+      flex: 1,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+      gap: spacing.sm,
+      padding: spacing.xs,
+      backgroundColor: Colors.neutral20,
+      borderRadius: radius.lg,
+    },
+    actionButtonText: {
+      fontSize: fontSizes.sm,
+      fontWeight: fontWeights.medium,
+      color: Colors.neutral90,
+      fontFamily: fontFamilies.primary,
+      fontStyle: "normal",
+      letterSpacing: 0.14,
+    },
+    loadingMoreContainer: {
+      paddingVertical: 20,
+      alignItems: "center",
+      gap: 8,
+    },
+    loadingMoreText: {
+      fontSize: 14,
+      color: Colors.textSecondary,
+    },
+    endOfListContainer: {
+      paddingVertical: 20,
+      alignItems: "center",
+    },
+    endOfListText: {
+      fontSize: 14,
+      color: Colors.textSecondary,
+    },
+    initialLoadingContainer: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      gap: 12,
+    },
+    initialLoadingText: {
+      fontSize: 16,
+      color: Colors.textSecondary,
+    },
+    emptyContainer: {
+      paddingVertical: 40,
+      alignItems: "center",
+    },
+    emptyText: {
+      fontSize: 16,
+      color: Colors.textSecondary,
+    },
+  })
